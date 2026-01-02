45d14000bcd11fabbf85c9e1d388d1ecb7c8c26f
Arturo Yion Jaime <arturoyj@Mac.local>
2026-01-02 15:50:52 -0600

    fix environment.ts

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   spotify: {
-    clientId: '7c2a4de796254c29adaa44207f806787',
-    clientSecret: '97b9abb5a5e94fa19432627e073af452', 
+    clientId: 'TU_CLIENT_ID_AQUI',
+    clientSecret: 'TU_CLIENT_SECRET_AQUI',
     authUrl: 'https://accounts.spotify.com/api/token',
     apiUrl: 'https://api.spotify.com/v1'
   }
